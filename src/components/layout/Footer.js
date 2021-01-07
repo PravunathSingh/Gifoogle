06a68fd0ef7e68887116f7ef52c2078ef51eb161
Pravunath Singh <pravunathsingh@gmail.com>
2021-01-07 22:04:42 +0530

    Added the Footer.js

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="p-2 bg-dark mt-5">
+            <p className="lead mt-3 text-white text-center">Developed by Pravunath Singh | Powered by Gifhy</p>
+        </footer>
+    )
+}
+
+export default Footer
