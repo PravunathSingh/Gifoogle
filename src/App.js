2358f9cdf41dff67a723b2fb90c4a389ef99472e
Pravunath Singh <pravunathsingh@gmail.com>
2021-01-07 22:02:53 +0530

    Added the App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/layout/Navbar';
+import Index from './components/layout/Index';
+import Footer from './components/layout/Footer';
+import {Provider} from './context';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Provider>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Index} />
+              </Switch>
+            </div>
+            <Footer />
+          </div>
+        </Router>
+      </Provider>
+    )
+  }
 }
 
 export default App;
