534561f39638e49cb5c530da4cfc81faf2c54a44
Pravunath Singh <pravunathsingh@gmail.com>
2021-01-07 22:06:18 +0530

    Added the Index.js

---
+++ src/components/layout/Index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Gifs from '../gifs/Gifs';
+import SearchGif from '../gifs/SearchGif';
+
+const Index = () => {
+    return (
+        <div>
+            <SearchGif />
+            <Gifs />
+        </div>
+    )
+}
+
+export default Index
